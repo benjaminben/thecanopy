ddd78bc14d09269abce20d5df93e53e533e5fcd9
benBen <benmbenjamin@gmail.com>
2016-10-03 14:46:13 -0700

    intro

--- compiled.js
+++ compiled.js
@@ -8,7 +8,61 @@ var boosh = 'booooo\nooooooooooooooooosh';
     deck: document.getElementById('deck')
   };
 
+  var Slide = function Slide(content) {
+    var cont = document.createElement('div'),
+        index = content.index;
+    cont.id = 'slide_' + index;
+    cont.className = 'slide';
+    cont.index = content.index;
+
+    cont.appendChild(content.img);
+
+    var text_cont = document.createElement('span'),
+        text = document.createElement('p');
+
+    text_cont.id = 'slide_' + index + '_text_cont';
+    text.id = 'slide_' + index + '_text';
+
+    text_cont.className = 'slide-text-cont';
+    text.className = 'slide-text';
+
+    text.innerHTML = content.text;
+
+    text_cont.appendChild(text);
+    cont.appendChild(text_cont);
+
+    cont.render = function () {
+      // cont.className += ' entering'
+      console.log('burf');
+      gs.deck.appendChild(cont);
+      gs.current_slide = slides[index];
+      setTimeout(function () {
+        window.addEventListener('wheel', wheelin);
+      }, 1000);
+    };
+
+    cont.transition = function (forward) {
+      if (forward && gs.current_slide === slides[slides.length - 1] || !forward && gs.current_slide === slides[0]) {
+        return false;
+      }
+
+      cont.className += ' exiting';
+      var next_slide = slides[forward ? index + 1 : index - 1];
+      setTimeout(function () {
+        gs.deck.removeChild(cont);
+        cont.className = cont.className.replace(/(?:^|\s)exiting(?!\S)/, '');
+        next_slide.render();
+      }, 1000);
+    };
+
+    return cont;
+  };
+
   var seeds = {
+    cover: {
+      background_img: './deck/slide_img_0.jpg',
+      title_img: ''
+    },
     slides: [{
       img_src: './deck/slide_img_0.jpg',
       text_src: './deck/0.txt'
@@ -40,6 +94,7 @@ var boosh = 'booooo\nooooooooooooooooosh';
   };
 
   var slides = seeds.slides.map(function (s, i) {
+    console.log('blub');
     return new Promise(function (resolve, reject) {
       var content_prom = new Promise(function (res, rej) {
         var img = new Image();
@@ -48,6 +103,8 @@ var boosh = 'booooo\nooooooooooooooooosh';
         img.onload = function () {
           res({ img: img, index: i });
         };
+
+        // resolve obj containing img
       }).then(function (obj) {
         var req = new XMLHttpRequest();
         req.open('GET', s.text_src);
@@ -66,74 +123,64 @@ var boosh = 'booooo\nooooooooooooooooosh';
             }
           }
         };
+        // resolve obj containing img_src and text_src
       });
     });
   });
 
   Promise.all(slides).then(function (ss) {
     slides = ss;
-    init(slides);
+    intro.cover();
     // console.log(ss)
   });
 
-  function Slide(content) {
-    var cont = document.createElement('div'),
-        index = content.index;
-    cont.id = 'slide_' + index;
-    cont.className = 'slide';
-    cont.index = content.index;
-
-    cont.appendChild(content.img);
-
-    var text_cont = document.createElement('span'),
-        text = document.createElement('p');
-
-    text_cont.id = 'slide_' + index + '_text_cont';
-    text.id = 'slide_' + index + '_text';
-
-    text_cont.className = 'slide-text-cont';
-    text.className = 'slide-text';
-
-    text.innerHTML = content.text;
-
-    text_cont.appendChild(text);
-    cont.appendChild(text_cont);
-
-    cont.render = function () {
-      // cont.className += ' entering'
-      gs.current_slide = cont;
-      gs.deck.appendChild(cont);
-      setTimeout(function () {
-        window.addEventListener('wheel', wheelin);
-      }, 1000);
-    };
-
-    cont.transition = function (forward) {
-      cont.className += ' exiting';
-      var next_slide = slides[(forward ? index + 1 : index - 1)];
-      setTimeout(function () {
-        gs.deck.removeChild(cont);
-        cont.className = cont.className.replace(/(?:^|\s)exiting(?!\S)/, '');
-        next_slide.render();
-      }, 1000);
-    };
-
-    return cont;
+  var intro = {
+    cover: function cover() {
+      var cover = document.createElement('div');
+      cover.style.backgroundColor = 'pink';
+      cover.id = 'cover';
+      document.body.appendChild(cover);
+      window.addEventListener('keydown', intro.enter);
+      return cover;
+    },
+    enter: function enter(e) {
+      if (e.keyCode === 32 || e.keyCode === 39) {
+        document.body.removeChild(document.getElementById('cover'));
+        init();
+        window.removeEventListener('keydown', intro.enter);
+      }
+    }
   };
 
-  function init(slides) {
-    gs.current_slide = slides[0];
-    slides[0].render();
-    window.addEventListener('wheel', wheelin);
+  function cover() {
+    window.addEventListener('keydown', enter);
   }
 
+  function init() {
+    gs.current_slide = slides[0];
+    slides[0].render();
+    bindKeys();
+  }
+
+  function bindKeys() {
+    window.addEventListener('wheel', wheelin);
+    window.addEventListener('keydown', keyin);
+  }
+  // function mousein(e){
+
+  // }
+  function keyin(e) {
+    if (e.keyCode === 32 || e.keyCode === 39) {
+      gs.current_slide.transition(true);
+    } else if (e.keyCode === 37) {
+      gs.current_slide.transition(false);
+    }
+  }
   function wheelin(e) {
     if (e.deltaY > 55 || e.deltaY < -55) {
       if (e.deltaY > 55) {
-        if( gs.current_slide === slides[slides.length - 1] ){ return }
         gs.current_slide.transition(true);
       } else if (e.deltaY < -55) {
-        if( gs.current_slide === slides[0] ){ return }
         gs.current_slide.transition(false);
       }
       window.removeEventListener('wheel', wheelin);
--- thecanopy.js
+++ thecanopy.js
@@ -35,14 +35,18 @@ var boosh = 'booooo\nooooooooooooooooosh';
       // cont.className += ' entering'
       console.log('burf')
       gs.deck.appendChild(cont);
+      gs.current_slide = slides[index];
       setTimeout(function(){
         window.addEventListener('wheel', wheelin);
       }, 1000);
     }
 
     cont.transition = function(forward){
+      if( (forward && gs.current_slide === slides[slides.length - 1]) ||
+          (!forward && gs.current_slide === slides[0]) ){return false}
+
       cont.className += ' exiting';
-      var next_slide = document.getElementById(`slide_${(forward ? index + 1 : index - 1)}`);
+      var next_slide = slides[(forward ? index + 1 : index - 1)];
       setTimeout(function(){
         gs.deck.removeChild(cont);
         cont.className = cont.className.replace( /(?:^|\s)exiting(?!\S)/ , '' )
@@ -54,6 +58,10 @@ var boosh = 'booooo\nooooooooooooooooosh';
   };
 
   var seeds = {
+    cover: {
+      background_img: './deck/slide_img_0.jpg',
+      title_img: '',
+    },
     slides: [{
       img_src: './deck/slide_img_0.jpg',
       text_src: './deck/0.txt'
@@ -120,16 +128,54 @@ var boosh = 'booooo\nooooooooooooooooosh';
   });
 
   Promise.all(slides).then(ss => {
-    init(ss);
+    slides = ss;
+    intro.cover();
     // console.log(ss)
   });
 
-  function init(slides){
-    gs.current_slide = slides[0];
-    slides[0].render();
-    window.addEventListener('wheel', wheelin);
+  var intro = {
+    cover: function(){
+      var cover = document.createElement('div');
+      cover.style.backgroundColor = 'pink';
+      cover.id = 'cover';
+      document.body.appendChild(cover);
+      window.addEventListener('keydown', intro.enter);
+      return cover;
+    },
+    enter: function(e){
+      if( e.keyCode === 32 || e.keyCode === 39 ){
+        document.body.removeChild(document.getElementById('cover'));
+        init();
+        window.removeEventListener('keydown', intro.enter);
+      }
+    },
   }
 
+  function cover(){
+    window.addEventListener('keydown', enter);
+  }
+
+  function init(){
+    gs.current_slide = slides[0];
+    slides[0].render();
+    bindKeys();
+  }
+
+  function bindKeys(){
+    window.addEventListener('wheel', wheelin);
+    window.addEventListener('keydown', keyin);
+  }
+  // function mousein(e){
+
+  // }
+  function keyin(e){
+    if( e.keyCode === 32 || e.keyCode === 39 ){
+      gs.current_slide.transition(true);
+    }
+    else if( e.keyCode === 37 ){
+      gs.current_slide.transition(false);
+    }
+  }
   function wheelin(e){
     if( e.deltaY > 55 || e.deltaY < -55 ){
       if( e.deltaY > 55 ){
@@ -142,205 +188,3 @@ var boosh = 'booooo\nooooooooooooooooosh';
     }
   }
 })();
-
-// // // creates a global "addWheelListener" method
-// // // example: addWheelListener( elem, function( e ) { console.log( e.deltaY ); e.preventDefault(); } );
-// // (function(window,document) {
-
-// //     var prefix = "", _addEventListener, support;
-
-// //     // detect event model
-// //     if ( window.addEventListener ) {
-// //         _addEventListener = "addEventListener";
-// //     } else {
-// //         _addEventListener = "attachEvent";
-// //         prefix = "on";
-// //     }
-
-// //     // detect available wheel event
-// //     support = "onwheel" in document.createElement("div") ? "wheel" : // Modern browsers support "wheel"
-// //               document.onmousewheel !== undefined ? "mousewheel" : // Webkit and IE support at least "mousewheel"
-// //               "DOMMouseScroll"; // let's assume that remaining browsers are older Firefox
-
-// //     window.addWheelListener = function( elem, callback, useCapture ) {
-// //         _addWheelListener( elem, support, callback, useCapture );
-
-// //         // handle MozMousePixelScroll in older Firefox
-// //         if( support == "DOMMouseScroll" ) {
-// //             _addWheelListener( elem, "MozMousePixelScroll", callback, useCapture );
-// //         }
-// //     };
-
-// //     function _addWheelListener( elem, eventName, callback, useCapture ) {
-// //         elem[ _addEventListener ]( prefix + eventName, support == "wheel" ? callback : function( originalEvent ) {
-// //             !originalEvent && ( originalEvent = window.event );
-
-// //             // create a normalized event object
-// //             var event = {
-// //                 // keep a ref to the original event object
-// //                 originalEvent: originalEvent,
-// //                 target: originalEvent.target || originalEvent.srcElement,
-// //                 type: "wheel",
-// //                 deltaMode: originalEvent.type == "MozMousePixelScroll" ? 0 : 1,
-// //                 deltaX: 0,
-// //                 deltaY: 0,
-// //                 deltaZ: 0,
-// //                 preventDefault: function() {
-// //                     originalEvent.preventDefault ?
-// //                         originalEvent.preventDefault() :
-// //                         originalEvent.returnValue = false;
-// //                 }
-// //             };
-
-// //             // calculate deltaY (and deltaX) according to the event
-// //             if ( support == "mousewheel" ) {
-// //                 event.deltaY = - 1/40 * originalEvent.wheelDelta;
-// //                 // Webkit also support wheelDeltaX
-// //                 originalEvent.wheelDeltaX && ( event.deltaX = - 1/40 * originalEvent.wheelDeltaX );
-// //             } else {
-// //                 event.deltaY = originalEvent.detail;
-// //             }
-
-// //             // it's time to fire the callback
-// //             return callback( event );
-
-// //         }, useCapture || false );
-// //     }
-
-// // })(window,document);
-
-// (function(){
-
-//   // function Slide(img_src, i){
-//   //   this.img_src = img_src;
-//   //   // this.text_src = text_src;
-//   //   this.text = (function(){
-//   //     var textReq = new XMLHttpRequest();
-//   //     textReq.open('GET', '/deck/'+i+'.txt');
-//   //     textReq.send(null);
-//   //     textReq.onreadystatechange = textReqReady;
-//   //   })();
-//   // }
-
-//   var slides = [],
-//       req = new XMLHttpRequest();
-
-//   var deck = {
-//     el : (function(){return document.getElementById('deck')})(),
-//     index : 0,
-//     renderSlide : function(up){
-//       console.log('blarf')
-//       var i = deck.index = (up ? deck.index - 1 : deck.index + 1);
-//       console.log(document.querySelector('img.current'));
-//       document.querySelector('img.current')[0].className = '';
-//       document.querySelector('img[data-index="'+i+'"]').className = 'current';
-
-//       setTimeout(function(){
-//         window.addEventListener('wheel', wheelin);
-//       }, 666);
-//     },
-//   };
-//     deck.up = deck.renderSlide(true),
-//     deck.down = deck.renderSlide(false);
-
-//   // var renderSlide = function(up){
-//   // }
-
-//   var init = function(total_slides) {
-//     for( var i = 0; i < total_slides; i++ ){
-//       var img = document.createElement('img');
-//       img.src = '/deck/slide_img_'+i+'.jpg';
-//       img.setAttribute('data-index', i)
-//       img.className = ('slide-img' + (i === 0 ? ' current' : ''));
-//       document.getElementById('deck').appendChild(img);
-//     }
-
-//     window.addEventListener('wheel', wheelin);
-//     function wheelin(e){
-
-//       if( e.deltaY > 55 || e.deltaY < -55 ){
-//         if( e.deltaY > 55 ){
-//           deck.down();
-//         }
-//         else if( e.deltaY < -55 ){
-//           deck.up();
-//         }
-//         window.removeEventListener('wheel', wheelin);
-//       }
-//     }
-//   };
-
-//   var initReqReady = function(){
-//     var req = this,
-//         DONE = 4,
-//         OK = 200;
-
-//     if( req.readyState === DONE ){
-//       if( req.status === OK ){
-//         var parser = new DOMParser(),
-//             doc = parser.parseFromString(req.response, 'text/html'),
-//             as = doc.getElementsByTagName('a'),
-//             total_slides = 0;
-
-//         for( var fi = 0; fi < as.length; fi++ ) {
-//           var href = as[fi].getAttribute('href');
-//           if( href.indexOf('slide_img') > -1 ){
-//             total_slides++;
-//           }
-//         }
-
-//         init(total_slides);
-//       }
-//     }
-
-//   //       // for( var si = 0; si < slides.length; si++ ){
-//   //       //   slides[si].text_src = '/deck/' + text_files[si];
-//   //       //   var textReq = new XMLHttpRequest();
-//   //       //   textReq.open('GET', slides[si].text_src);
-//   //       //   textReq.send(null);
-//   //       //   textReq.onreadystatechange = textReqReady;
-//   //       // }
-//   //     }
-//       else {
-//         console.log('Error: ' + req.status, req);
-//       }
-//     }
-
-//   req.open('GET', '/deck');
-//   req.send(null);
-
-//   req.onreadystatechange = initReqReady;
-//   //   }
-//   // }
-
-//   // function textReqReady(){
-//   //   var text_req = this,
-//   //       DONE = 4,
-//   //       OK = 200;
-
-//   //   if( text_req.readyState === DONE ){
-//   //     console.log('bust')
-//   //     if( text_req.status === OK ){
-//   //       return text_req.responseText;
-//   //     }
-//   //     else {
-//   //       console.log('Error: ' + text_req.status, text_req);
-//   //     }
-//   //   }
-//   // }
-
-
-//   // var Slide = function(img_src, text){
-
-//   // }
-
-//   // var slides = [];
-
-//   // for( var i = 0; i < 10; i++ ){
-//   //   // new Slide('/deck/'+i+'');
-//   //   var text_req = new XMLHttpRequest();
-//   //   text_req.open('GET', '/deck/'+i+'.txt');
-//   //   text_req.send(null);
-//   //   text_req.onreadystatechange = textReqReady;
-//   // }
-// })();
